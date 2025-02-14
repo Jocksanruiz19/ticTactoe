3e28c0f4592972851de2ac3f96ac1e2e1e4062f8
Jocksanruiz19 <Jocksanruiz18@gmail.com>
2025-02-14 12:38:56 -0600

    avance proyecto 14/2/24

--- src/js/funcionalidad.js
+++ src/js/funcionalidad.js
@@ -1,15 +1,30 @@
-const contenedor = document.getElementsByClassName("contenedor")
-const Reiniciar = document.getElementsByClassName("Reiniciar")
-
-
+const contenedor = document.getElementsByClassName("contenedor");
+const Reiniciar = document.getElementsByClassName("Reiniciar")[0];
 
 for (let index = 0; index < contenedor.length; index++) {
-   
-   contenedor[index].addEventListener("click", function(){
+  contenedor[index].addEventListener("click", function () {
+    if (contenedor[index].textContent !== "") {
+      return;
+    }
+
     contenedor[index].textContent = "X";
-  })
-  
+
+    let espaciosLibres = [];
+    for (let i = 0; i < contenedor.length; i++) {
+      if (contenedor[i].textContent === "") {
+        espaciosLibres.push(i);
+      }
+    }
+
+    if (espaciosLibres.length > 0) {
+      let aleatorio = Math.floor(Math.random() * espaciosLibres.length);
+      contenedor[espaciosLibres[aleatorio]].textContent = "O";
+    }
+  });
 }
-let marcador = [""]
 
-
+Reiniciar.addEventListener("click", function () {
+  for (let index = 0; index < contenedor.length; index++) {
+    contenedor[index].textContent = "";
+  }
+});
